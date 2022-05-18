56c52e2e7a70e84c330b5430ab0f33bcfdc4361a
Matthew-Dreemurr <hadadmat@gmail.com>
2022-05-18 14:32:26 +0200

    fix

--- src/minishell.c
+++ src/minishell.c
@@ -6,7 +6,7 @@
 /*   By: mahadad <mahadad@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/11 10:16:20 by awillems          #+#    #+#             */
-/*   Updated: 2022/05/18 12:18:27 by mahadad          ###   ########.fr       */
+/*   Updated: 2022/05/18 14:28:15 by mahadad          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -69,7 +69,6 @@ int	main(int argc, char *argv[])
 	signal(SIGINT, sighandler);
 	signal(SIGQUIT, sighandler);
 
-	sigaction()
 
 	prompt();
 	//TODO make custom exit function that free stuff befor quit
--- src/msh_parser.c
+++ src/msh_parser.c
@@ -1,22 +1,13 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   parser.c                                           :+:      :+:    :+:   */
+/*   msh_parser.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: awillems <awillems@student.42.fr>          +#+  +:+       +#+        */
+/*   By: mahadad <mahadad@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/16 13:55:33 by mahadad           #+#    #+#             */
-/*   Updated: 2022/05/17 15:24:42 by awillems         ###   ########.fr       */
+/*   Updated: 2022/05/18 14:28:37 by mahadad          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
-
-int	main(void)
-{
-	t_vector line = init_vector_str("Lorem ipsum dolor sit amet. Rem commodi earum");
-	char *line_read = readline("[test] ");
-
-	print_vector(line);
-	insert_vector(&line, line_read, 10);
-}
