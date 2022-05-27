4276200f7f70d2635a90dc6d246d26ddf40aa6f7
willaCS <arthur.willems.prive@gmail.com>
2022-05-27 16:47:02 +0200

    wip lexer
    Co-authored-by: willa <willaCS@users.noreply.github.com>

--- src/1_line_lexer/line_param_func.c
+++ src/1_line_lexer/line_param_func.c
@@ -6,7 +6,7 @@
 /*   By: mahadad <mahadad@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/27 13:25:32 by awillems          #+#    #+#             */
-/*   Updated: 2022/05/27 14:40:50 by mahadad          ###   ########.fr       */
+/*   Updated: 2022/05/27 16:45:08 by mahadad          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@
 #include <stdio.h>
 
 /**
- * @brief 
+ * @brief //TODO
  */
 int whtspc(t_vec *vec, char *line, int index)
 {
@@ -26,25 +26,41 @@ int whtspc(t_vec *vec, char *line, int index)
 }
 
 /**
- * @brief 
+ * @brief When a `$` is occur, will check if the next character is `alphanum`,
+ *        make `getenv` and push the variable content inside line buffer.   If
+ *        the character after `$` is `!alphanum` will push `$` in line buffer.
  */
 int varsub(t_vec *vec, char *line, int index)
 {
-	int	len;
+	int		len;
+	char	*env_str;
 
-	(void) line;
-	(void) vec;
 	len = 0;
+	// Skip `$`
 	index++;
-	while (ft_isalnum(line[index]))
+	// Check if the next char is alphanum
+	if (!ft_isalnum(line[index]))
+	{
+		// If not, push `$` and return the index of the char after `$`.
+		vec_fill(vec, FIXED_LEN, "$", 1);
+		return (index);
+	}
+	// strlen of the var name
+	while (ft_isalnum(line[index + len]))
 		len++;
-	if (ft_strchr(" \t\n\v\f\r", line[index]))
-		return (index+1);
-	return (index+1);
+	// Put the var name in tmp buffer
+	vec_delete_content(&g_data.tmp);
+	vec_fill(&g_data.tmp, FIXED_LEN, &line[index], len);
+	// Find the env var with the tmp buffer
+	env_str = getenv(g_data.tmp.buffer);
+	// If var find push the content in line buffer
+	if (env_str)
+		vec_fill(vec, DEFAULT, env_str);
+	return (index + len);
 }
 
 /**
- * @brief 
+ * @brief //TODO
  */
 int bkslh(t_vec *vec, char *line, int index)
 {
@@ -60,7 +76,33 @@ int bkslh(t_vec *vec, char *line, int index)
  */
 int sglqot(t_vec *vec, char *line, int index)
 {
-	(void) line;
-	(void) vec;
+	vec_fill(vec, FIXED_LEN, '\'', 1);
+	index++;
+	while (line[index] && line[index] != '\'')
+	{
+		vec_fill(vec, FIXED_LEN, &line[index], 1);
+		index++;
+	}
+	if (!line[index])
+		return (index);
+	vec_fill(vec, FIXED_LEN, '\'', 1);
+	return (index + 1);
+}
+
+/**
+ * @brief 
+ */
+int dblqot(t_vec *vec, char *line, int index)
+{
+	vec_fill(vec, FIXED_LEN, '\'', 1);
+	index++;
+	while (line[index] && line[index] != '\"')
+	{
+		vec_fill(vec, FIXED_LEN, &line[index], 1);
+		index++;
+	}
+	if (!line[index])
+		return (index);
+	vec_fill(vec, FIXED_LEN, '\'', 1);
 	return (index + 1);
 }
