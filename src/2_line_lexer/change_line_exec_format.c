d78582a54e4770d2e980d313fb48b5a2bfc88079
matthew-dreemurr <hadadmat@gmail.com>
2022-06-01 10:47:20 +0200

    wip

--- src/2_line_lexer/change_line_exec_format.c
+++ src/2_line_lexer/change_line_exec_format.c
@@ -6,7 +6,7 @@
 /*   By: mahadad <mahadad@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/30 10:10:31 by awillems          #+#    #+#             */
-/*   Updated: 2022/05/31 17:38:26 by mahadad          ###   ########.fr       */
+/*   Updated: 2022/06/01 10:38:51 by mahadad          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,19 +35,21 @@ void	create_new_instr(int type, t_vec *instr)
 char *find_bin_path(char *line)
 {
 	(void)line;
-	return ("WIP/bin");
+	return ("WIP/bin/");
 }
 
-void	set_bin_path(t_vec *arg, char *line)
+void	set_bin_path(t_vec *arg, t_vec *line, int index)
 {
 	// Check if the CMD is a bin with path
-	if (access(line, X_OK) != -1)
+	if (access(vec_get_str(line, index), X_OK) != -1)
 	{
 		vec_add(arg, line);
 		return ;
 	}
 	// find the good path
-	vec_add(arg, find_bin_path(line));
+	// vec_add(arg, find_bin_path(line));
+	vec_insert(line, DEFAULT, index, find_bin_path(vec_get_str(line, index)));
+	vec_print(line);
 }
 
 void	reset_instr(void)
@@ -64,6 +66,41 @@ void	reset_instr(void)
  * //TODO
  * //TODO
  */
+
+/**
+ * @brief Get the arg vector.
+ * 
+ * @param index The index of element.
+ * @return t_vec* Return a ptr to the vector.
+ */
+t_vec	*get_instr_arg(int index)
+{
+	return &(vec_get_instr(&g_data.lexed_instr, index)->arg);
+}
+
+static void	instr_debug(void)
+{
+	printf("[\n");
+	for (size_t x = 0; x < g_data.lexed_instr.content_len; x++)
+	{
+		printf("    {\n");
+		printf("        type: %d,\n", vec_get_instr(&g_data.lexed_instr, x)->type);
+		size_t owo = 0;
+		size_t uwu = get_instr_arg(x)->content_len;
+		printf("        content_len: %lu,\n", uwu);
+		printf("        arg: [\n");
+		while (uwu)
+		{
+			printf("            [%lu]: \"%s\",\n",
+					owo, vec_get_str(get_instr_arg(x), owo));
+			uwu--;
+			owo++;
+		}
+		printf("        ],\n");
+		printf("    },\n");
+	}
+	printf("]\n");
+}
 
 void	change_line_to_exec_format(t_vec *line, t_vec *instr)
 {
@@ -86,7 +123,7 @@ void	change_line_to_exec_format(t_vec *line, t_vec *instr)
 				coun_elem++;
 				printf("PIPE ");
 				create_new_instr(2, instr);
-				vec_add(&(vec_get_instr(&g_data.lexed_instr, coun_elem)->arg), &line->buffer[i]);
+				vec_add((get_instr_arg(coun_elem)), line->buffer + i);
 				first_elem = 1;
 			}
 			else
@@ -95,38 +132,19 @@ void	change_line_to_exec_format(t_vec *line, t_vec *instr)
 				{
 					coun_elem++;
 					create_new_instr(0, instr);
-					set_bin_path(&(vec_get_instr(&g_data.lexed_instr, coun_elem)->arg), &line->buffer[i]);
+					set_bin_path(get_instr_arg(coun_elem), line, i);
 					printf("CMD ");
 					first_elem = 0;
 				}
 				else
 				{
-					vec_add(&(vec_get_instr(&g_data.lexed_instr, coun_elem)->arg), &line->buffer[i]);
+					vec_add(get_instr_arg(coun_elem), line->buffer + i);
 					printf("ARG ");
 				}
 			}
-			printf("[%d]=> [%s]\n", coun_elem, &line->buffer[i]);
+			printf("[%d]=> [%s]\n", coun_elem, line->buffer + i);
 		}
 		i++;
 	}
-	for (size_t x = 0; x < g_data.lexed_instr.content_len; x++)
-	{
-		printf("[\n");
-		printf("    type: %d,\n", vec_get_instr(&g_data.lexed_instr, x)->type);
-		size_t owo = 0;
-		size_t uwu = vec_get_instr(&g_data.lexed_instr, x)->arg.content_len;
-		printf("    content_len: %lu,\n", uwu);
-
-		printf("    arg: {\n");
-		while (uwu)
-		{
-			printf("        [%lu]: \"%s\"\n",
-					owo, vec_get_str(&(vec_get_instr(&g_data.lexed_instr, x)->arg), owo));
-			uwu--;
-			owo++;
-		}
-		printf("    },\n");
-		printf("],\n");
-	}
-	
+	instr_debug();
 }
