02b1dc4656ae13fd39789805e892baa1eff5e6e8
willaCS <arthur.willems.prive@gmail.com>
2022-06-02 11:18:51 +0200

    start line exe

--- src/3_line_executer/line_executer.c
+++ src/3_line_executer/line_executer.c
@@ -1,29 +1,19 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   interpret_line.c                                   :+:      :+:    :+:   */
+/*   line_executer.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: awillems <awillems@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/05/27 09:59:33 by awillems          #+#    #+#             */
-/*   Updated: 2022/06/02 10:54:24 by awillems         ###   ########.fr       */
+/*   Created: 2022/06/02 10:52:02 by awillems          #+#    #+#             */
+/*   Updated: 2022/06/02 11:12:48 by awillems         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
-#include <stdio.h>
 
-void	line_cleaner(t_vec *vec, char *line);
-void	line_lexer(t_vec *line, t_vec *instr);
-void	line_executer(t_vec *instr);
-
-int	interpret_line(char *line)
+void	line_executer(t_vec *instr)
 {
-	line_cleaner(&g_data.lexed_command, line);
-	line_lexer(&g_data.lexed_command, &g_data.lexed_instr);
-	line_executer(&g_data.lexed_instr);
-
-	vec_delete(&g_data.lexed_command);
-	vec_delete(&g_data.lexed_instr);
-	return (1);
+	(void) instr;
+	return ;
 }
