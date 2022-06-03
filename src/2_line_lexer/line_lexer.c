c9b8b47c189f5aac52217cc05c616dc51b20414c
willaCS <arthur.willems.prive@gmail.com>
2022-06-03 14:38:27 +0200

    better

--- src/2_line_lexer/line_lexer.c
+++ src/2_line_lexer/line_lexer.c
@@ -6,7 +6,7 @@
 /*   By: awillems <awillems@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/30 10:10:31 by awillems          #+#    #+#             */
-/*   Updated: 2022/06/03 14:26:55 by awillems         ###   ########.fr       */
+/*   Updated: 2022/06/03 14:37:08 by awillems         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,12 +21,12 @@ void	print_instr(size_t len);
 int		is_special_elem(char *elem);
 void	set_bin_path(t_vec *line, int index);
 
-void	add_arg(t_vec* instr, size_t index, char *arg)
+static void	add_arg(t_vec* instr, size_t index, char *arg)
 {
 	vec_add_char_ptr(&vec_get_instr(instr, index)->arg, arg);
 }
 
-void	new_instr(t_vec* instr, size_t index, int type, char *arg)
+static void	new_instr(t_vec* instr, size_t index, int type, char *arg)
 {
 	t_instr new_instr;
 	
@@ -42,31 +42,38 @@ void	new_instr(t_vec* instr, size_t index, int type, char *arg)
 	add_arg(instr, index, arg);
 }
 
-void	line_lexer(t_vec *line, t_vec *instr)
+/**
+ * @param is_cmd HAS TO BE 1
+ * @param cmd_index HAS TO BE -1
+ */
+void	line_lexer(t_vec *line, t_vec *instr, int is_cmd, int cmd_index)
 {
-	int	is_cmd = 1;
-	int	cmd_index = -1;
-	for (size_t i = 0; i < line->content_len; i++)
+	size_t	i;
+
+	i = 0;
+	while (i < line->content_len)
 	{
-		// if (!(line[i] != 0 && (i == 0 || line[i-1] == 0)))
-		if (!(vec_get_char(line, i) != 0 && (i == 0 || vec_get_char(line, i - 1) == 0)))
-			continue;
-		if (is_special_elem(vec_get_str(line, i)) != 0)
+		if (i == 0 || vec_get_char(line, i - 1) == 0)
 		{
-			cmd_index++;
-			is_cmd = 1;
-			/* /!\ insert path /!\ */
-			new_instr(instr, cmd_index, 2, vec_get_str(line, i));
+			if (is_special_elem(vec_get_str(line, i)) != 0)
+			{
+				cmd_index++;
+				is_cmd = 1;
+				/* /!\ insert path /!\ */
+				new_instr(instr, cmd_index, 2, vec_get_str(line, i));
+			}
+			else if (is_cmd)
+			{
+				cmd_index++;
+				is_cmd = 0;
+				new_instr(instr, cmd_index, 1, vec_get_str(line, i));
+			}
+			else
+				add_arg(instr, cmd_index, vec_get_str(line, i));
 		}
-		else if (is_cmd)
-		{
-			cmd_index++;
-			is_cmd = 0;
-			new_instr(instr, cmd_index, 1, vec_get_str(line, i));
-		}
-		else
-			add_arg(instr, cmd_index, vec_get_str(line, i));
+		i++;
 	}
-	// printf("\n\n");
-	// print_instr(cmd_index + 1);
+	printf("\n\n");
+	print_instr(cmd_index + 1);
+	printf("\n\n");
 }
--- src/interpret_line.c
+++ src/interpret_line.c
@@ -6,7 +6,7 @@
 /*   By: awillems <awillems@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/27 09:59:33 by awillems          #+#    #+#             */
-/*   Updated: 2022/06/03 14:21:34 by awillems         ###   ########.fr       */
+/*   Updated: 2022/06/03 14:36:57 by awillems         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,16 +15,14 @@
 #include "vec_utils.h" /* vec_get_instr */
 
 void	line_cleaner(t_vec *vec, char *line);
-void	line_lexer(t_vec *Line, t_vec *instr);
-
-void	line_lexer(t_vec *line, t_vec *instr);
+void	line_lexer(t_vec *line, t_vec *instr, int is_cmd, int cmd_index);
 void	line_executer(t_vec *instr);
 void	empty_instr(t_vec *instr);
 
 int	interpret_line(char *line)
 {
 	line_cleaner(&g_data.lexed_command, line);
-	line_lexer(&g_data.lexed_command, &g_data.lexed_instr);
+	line_lexer(&g_data.lexed_command, &g_data.lexed_instr, 1, -1);
 	line_executer(&g_data.lexed_instr);
 
 	vec_delete(&g_data.lexed_command);
