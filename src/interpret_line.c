d1b553ad3c51d4ab9c26e244677233c14d5daa04
willaCS <arthur.willems.prive@gmail.com>
2022-06-03 14:41:16 +0200

    abc

--- src/interpret_line.c
+++ src/interpret_line.c
@@ -6,7 +6,7 @@
 /*   By: awillems <awillems@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/27 09:59:33 by awillems          #+#    #+#             */
-/*   Updated: 2022/06/03 14:36:57 by awillems         ###   ########.fr       */
+/*   Updated: 2022/06/03 14:41:07 by awillems         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@
 void	line_cleaner(t_vec *vec, char *line);
 void	line_lexer(t_vec *line, t_vec *instr, int is_cmd, int cmd_index);
 void	line_executer(t_vec *instr);
-void	empty_instr(t_vec *instr);
+void	empty_g_data(void);
 
 int	interpret_line(char *line)
 {
@@ -25,7 +25,6 @@ int	interpret_line(char *line)
 	line_lexer(&g_data.lexed_command, &g_data.lexed_instr, 1, -1);
 	line_executer(&g_data.lexed_instr);
 
-	vec_delete(&g_data.lexed_command);
-	empty_instr(&g_data.lexed_instr);
+	empty_g_data();
 	return (1);
 }
