2bbe599a84e018fd535e45c5aa21824df2d7c406
willaCS <arthur.willems.prive@gmail.com>
2022-06-06 11:57:33 +0200

    fixed print _instr

--- src/2_line_lexer/print_instr.c
+++ src/2_line_lexer/print_instr.c
@@ -3,39 +3,28 @@
 /*                                                        :::      ::::::::   */
 /*   print_instr.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mahadad <mahadad@student.s19.be>           +#+  +:+       +#+        */
+/*   By: awillems <awillems@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/02 11:20:44 by awillems          #+#    #+#             */
+/*   Updated: 2022/06/06 11:57:19 by awillems         ###   ########.fr       */
 /*                                                                            */
+/* ************************************************************************** */
+
 #include "vec_utils.h"
 #include "stdio.h"
 
-t_vec	*get_instr_arg(int index);
-
 void	print_instr(size_t len)
 {
-	t_instr	*instr;
-	
 	printf("[\n");
-		printf("    content_len: %lu,\n", g_data.lexed_instr.content_len);
-	for (size_t x = 0; x < len; x++)
+	printf("    content_len: %lu,\n", get_instr_list()->content_len);
+	for (size_t ind = 0; ind < len; ind++)
 	{
-		instr = vec_get_instr(&g_data.lexed_instr, x);
-		printf("    {\n");
-		printf("        type: %d,\n", instr->type);
-		size_t x = 0;
-		size_t y = instr->arg.content_len;
-		printf("        content_len: %lu,\n", y);
-		printf("        arg: [\n");
-		while (y)
-		{
-			printf("            [%lu]: \"%s\",\n", x,
-				vec_get_str_array(&instr->arg, x));
-			y--;
-			x++;
-		}
-		printf("        ]\n");
-		printf("    },\n");
+	printf("    {\n");
+	printf("        type: %d,\n", get_instr(ind)->type);
+	printf("        arg: [\n");
+	for (size_t arg = 0; arg < get_instr_arg(ind)->content_len; arg++)
+	printf("            [%lu]: \"%s\",\n", arg, get_instr_arg_elem(ind, arg));
+	printf("        ]\n");
 	}
 	printf("]\n");
 }
