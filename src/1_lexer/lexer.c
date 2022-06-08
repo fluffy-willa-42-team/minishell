7113eaaabf90d995495bf720bcb59a07dc74d8b1
willaCS <arthur.willems.prive@gmail.com>
2022-06-08 14:31:37 +0200

    restart lexer

--- src/1_lexer/lexer.c
+++ src/1_lexer/lexer.c
@@ -6,14 +6,64 @@
 /*   By: awillems <awillems@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/08 13:14:02 by awillems          #+#    #+#             */
-/*   Updated: 2022/06/08 13:41:28 by awillems         ###   ########.fr       */
+/*   Updated: 2022/06/08 14:31:11 by awillems         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 #include "vec_utils.h"
 
+int	bkslh(void);
+int	sglqot(void);
+int	dblqot(void);
+int	varsub(void);
+int	whtspc(void);
+int	spec_0_arg(void);
+int	spec_1_arg(void);
+int	spec_scndry_prompt(void);
+
+int	get_elem(char *str)
+{
+	const char *spec_char[] = {
+		"\\",
+		"\'", "\"",
+		"$",
+		" ", "\t","\n", "\v","\f", "\r",
+		"|", ";",
+		"<", ">",
+		"<<", ">>",
+	};
+	
+	for (int j = 0; j < 14; j++){
+		if (*spec_char[j] == *str)
+		{
+			if (ft_strncmp(spec_char[j + 2], str, 2) == 0)
+				return (j + 2);
+			return (j);
+		}
+	}
+	return (-1);
+}
+
 void	line_lexer(char *line)
 {
-	printf("%s\n", line);
+	static int	(*func_link[])() = {
+		bkslh,
+		sglqot, dblqot,
+		varsub,
+		whtspc, whtspc, whtspc, whtspc, whtspc, whtspc,
+		spec_0_arg, spec_0_arg,
+		spec_1_arg, spec_1_arg,
+		spec_scndry_prompt, spec_1_arg,
+	};
+	
+	
+	int i = 0;
+	while (line[i])
+	{
+		printf("[%d]\t\'%c\'\t=>\t%d\n", i, line[i], get_elem(&line[i]));
+		if (get_elem(&line[i]) != -1)
+			func_link[get_elem(&line[i])]();
+		i++;
+	}
 }
---
+++ src/1_lexer/lexer_param_func.c
@@ -0,0 +1,53 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   lexer_param_func.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: awillems <awillems@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/08 14:22:55 by awillems          #+#    #+#             */
+/*   Updated: 2022/06/08 14:30:11 by awillems         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+int bkslh(void)
+{
+	return (1);
+}
+
+int sglqot(void)
+{
+	return (1);
+}
+
+int dblqot(void)
+{
+	return (1);
+}
+
+int varsub(void)
+{
+	return (1);
+}
+
+int whtspc(void)
+{
+	return (1);
+}
+
+int spec_0_arg(void)
+{
+	return (1);
+}
+
+int spec_1_arg(void)
+{
+	return (1);
+}
+
+int spec_scndry_prompt(void)
+{
+	return (1);
+}
