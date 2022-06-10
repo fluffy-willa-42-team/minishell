dd807f7c9a82ed4ff913cd6e25de69fd078a2cf3
willaCS <arthur.willems.prive@gmail.com>
2022-06-10 12:57:52 +0200

    norm

--- src/1_lexer/lexer.c
+++ src/1_lexer/lexer.c
@@ -6,7 +6,7 @@
 /*   By: awillems <awillems@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/08 13:14:02 by awillems          #+#    #+#             */
-/*   Updated: 2022/06/10 12:52:59 by awillems         ###   ########.fr       */
+/*   Updated: 2022/06/10 12:57:40 by awillems         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -49,23 +49,26 @@ static	void	init_opt(t_lexer_opt *opt)
 
 lexer_param	lexer_param_func(char *str)
 {
-	const char *spec_char[] = {
-		"\\", "\'", "\"", "$", " ", "\t", "\n", "\v","\f", "\r", "|", ";", "<",
+	const char			*spec_char[] = {
+		"\\", "\'", "\"", "$", " ", "\t", "\n", "\v", "\f", "\r", "|", ";", "<",
 		">", "<<", ">>",
 	};
-	const lexer_param func_link[] = {
+	const lexer_param	func_link[] = {
 		bkslh, sglqot, dblqot, varsub, whtspc, whtspc, whtspc, whtspc, whtspc,
 		whtspc, spec_0_arg, spec_0_arg, spec_1_arg, spec_1_arg,
 		spec_scndry_prompt, spec_1_arg,
 	};
-	
-	for (int i = 0; i < 14; i++){
+	int					i;
+
+	while (i < 14)
+	{
 		if (*spec_char[i] == *str)
 		{
 			if (i >= 12 && ft_strncmp(spec_char[i + 2], str, 2) == 0)
 				return (func_link[i + 2]);
 			return (func_link[i]);
 		}
+		i++;
 	}
 	return (dflt_char);
 }
@@ -73,9 +76,10 @@ lexer_param	lexer_param_func(char *str)
 void	line_lexer(char *line)
 {
 	t_lexer_opt	opt;
+	int			i;
 
 	init_opt(&opt);
-	int i = 0;
+	i = 0;
 	while (line[i] && ft_is_whitespace(line[i]))
 		i++;
 	while (line[i])
