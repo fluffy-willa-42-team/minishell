9445f2b5dd7daa8cfbfab4eda9c09b9fd1633393
willaCS <arthur.willems.prive@gmail.com>
2022-06-10 10:38:12 +0200

    a

--- src/1_lexer/lexer_param_func.c
+++ src/1_lexer/lexer_param_func.c
@@ -6,27 +6,27 @@
 /*   By: awillems <awillems@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/08 14:22:55 by awillems          #+#    #+#             */
-/*   Updated: 2022/06/10 10:25:56 by awillems         ###   ########.fr       */
+/*   Updated: 2022/06/10 10:36:42 by awillems         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "msh_lexer.h"
 
 void	add_char(t_lexer_opt *opt, char *arg);
-void	add_arg(t_lexer_opt *opt, int index);
-void	new_instr(t_lexer_opt *opt, int type, int index);
+void	add_arg(t_lexer_opt *opt);
+void	new_instr(t_lexer_opt *opt, int type);
 
 int dflt_char(char *line, int index, t_lexer_opt *opt)
 {
 	add_char(opt, &line[index]);
 	if (opt->new_instr == 1)
 	{
-		new_instr(opt, 1, index);
+		new_instr(opt, 1);
 		opt->new_instr = 0;
 	}
 	else if (opt->new_arg == 1)
 	{
-		add_arg(opt, index);
+		add_arg(opt);
 		opt->new_arg = 0;
 	}
 	return (1);
@@ -71,16 +71,21 @@ int whtspc(char *line, int index, t_lexer_opt *opt)
 	i = 0;
 	while (line[index + i] && ft_is_whitespace(line[index + i]))
 		i++;
-	add_char(opt, "\0");
+	if (!opt->new_arg)
+		add_char(opt, "\0");
 	opt->new_arg = 1;
 	return (i);
 }
 
 int spec_0_arg(char *line, int index, t_lexer_opt *opt)
 {
-	(void) line;
-	(void) index;
-	(void) opt;
+	if (!opt->new_arg)
+		add_char(opt, "\0");
+	add_char(opt, &line[index]);
+	new_instr(opt, 2);
+	add_char(opt, "\0");
+	opt->new_instr = 1;
+	opt->new_arg = 1;
 	return (1);
 }
 
