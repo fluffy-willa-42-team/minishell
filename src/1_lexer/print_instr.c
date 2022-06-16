01d1b6554adcf3c0520481a3748b80843edad6cd
willaCS <arthur.willems.prive@gmail.com>
2022-06-16 13:49:43 +0200

    updtae print

--- src/1_lexer/print_instr.c
+++ src/1_lexer/print_instr.c
@@ -6,7 +6,7 @@
 /*   By: awillems <awillems@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/02 11:20:44 by awillems          #+#    #+#             */
-/*   Updated: 2022/06/16 13:35:33 by awillems         ###   ########.fr       */
+/*   Updated: 2022/06/16 13:49:28 by awillems         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,14 +19,14 @@ void	print_instr(size_t len)
 	printf("instr_list: [\n");
 	for (size_t ind = 0; ind < len; ind++)
 	{
-	printf("    {\n");
-	printf("        type: %d,\n", get_instr(ind)->type);
-	printf("        content_len: %lu,\n", get_instr_arg(ind)->content_len);
-	printf("        arg: [\n");
-	for (size_t arg = 0; arg < get_instr_arg(ind)->content_len; arg++)
-	printf("            [%lu]: \"%s\",\n", arg, get_instr_arg_elem(ind, arg));
-	printf("        ]\n");
-	printf("    },\n");
+		printf("\t{\n");
+		printf("\t\ttype: %d,\n", get_instr(ind)->type);
+		printf("\t\tcontent_len: %lu,\n", get_instr_arg(ind)->content_len);
+		printf("\t\targ: [\n");
+		for (size_t arg = 0; arg < get_instr_arg(ind)->content_len; arg++)
+			printf("\t\t\t[%lu]: \"%s\",\n", arg, get_instr_arg_elem(ind, arg));
+		printf("\t\t]\n");
+		printf("\t},\n");
 	}
 	printf("]\n\n");
 }
