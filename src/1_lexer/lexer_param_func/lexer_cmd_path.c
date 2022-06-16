4506b561d0a2726a32f6519dc9bf8865616a2b9f
willaCS <arthur.willems.prive@gmail.com>
2022-06-16 22:02:54 +0200

    got to sleep young one

--- src/1_lexer/lexer_param_func/lexer_cmd_path.c
+++ src/1_lexer/lexer_param_func/lexer_cmd_path.c
@@ -6,15 +6,19 @@
 /*   By: awillems <awillems@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/16 18:48:49 by awillems          #+#    #+#             */
-/*   Updated: 2022/06/16 19:40:00 by awillems         ###   ########.fr       */
+/*   Updated: 2022/06/16 22:02:35 by awillems         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "msh_lexer.h"
 
-void	add_cmd_path(t_lexer_opt *opt, int index)
+void	add_cmd_path(t_lexer_opt *opt, int index_instr)
 {
+	const int	index = *((int *) vec_get(get_instr_arg(index_instr), 0));
+	
+	// printf("%ld - %ld -1 => %ld\n", opt->nb_instr, opt->index_instr, opt->nb_instr - opt->index_instr - 1);
+	// if ()
 	(void) opt;
-	printf("VAR FINISHED => %s\n",
-		get_line()->buffer + *((int *) vec_get(get_instr_arg(index), 0)));
+	printf("VAR FINISHED => %s\n", vec_get_str(get_line(), index));
 }
+// mdr ; abc << def hij
