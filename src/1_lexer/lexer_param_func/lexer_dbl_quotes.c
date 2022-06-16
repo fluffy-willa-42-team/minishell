61d7a1c643a3b6ecd389ea8bb41757308d1f8bdc
willaCS <arthur.willems.prive@gmail.com>
2022-06-16 11:31:40 +0200

    start rework

---
+++ src/1_lexer/lexer_param_func/lexer_dbl_quotes.c
@@ -0,0 +1,54 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   lexer_dbl_quotes.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: awillems <awillems@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/16 11:19:03 by awillems          #+#    #+#             */
+/*   Updated: 2022/06/16 11:31:16 by awillems         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "msh_lexer.h"
+
+void	add_instr_or_arg(t_lexer_opt *opt);
+int		add_varsub(char *line, int index, t_lexer_opt *opt);
+
+static int	add_dblqot(char *line, int index, t_lexer_opt *opt)
+{
+	int	i;
+
+	i = 0;
+	while (line[++i + index] && line[index + i] != '\"')
+	{
+		if (line[index + i] == '\\' && line[index + i] && line[index + i + 1] != '\\')
+		{
+			add_char(opt, &line[index + i + 1]);
+			if (line[index + i + 1])
+				i++;
+		}
+		else if (line[index + i] == '$')
+			i += add_varsub(line, index + i, opt) - 1;
+		else
+			add_char(opt, &line[index + i]);
+	}
+	return (i);
+}
+
+/** " */
+int dblqot(char *line, int index, t_lexer_opt *opt)
+{
+	int	i;
+
+	opt->index_line++;
+	add_instr_or_arg(opt);
+	opt->index_line--;
+	i = add_dblqot(line, index, opt);
+	if (line[index + i] != '\"')
+	{
+		printf("/!\\ unclosed double quote\n");//TODO
+		return (i);
+	}
+	return (i + 1);
+}
---
+++ src/1_lexer/lexer_param_func/lexer_var_sub.c
@@ -0,0 +1,66 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   lexer_var_sub.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: awillems <awillems@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/10 12:26:12 by awillems          #+#    #+#             */
+/*   Updated: 2022/06/16 11:29:56 by awillems         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "msh_lexer.h"
+
+void	add_instr_or_arg(t_lexer_opt *opt)
+{
+	if (opt->new_instr == 1)
+	{
+		new_instr(opt, 1);
+		opt->new_instr = 0;
+	}
+	else if (opt->new_arg == 1)
+		add_arg(opt);
+}
+
+int	get_var_len(char *line, int index)
+{
+	int	len;
+	
+	len = 0;
+	while (ft_isalpha(line[index + len]) || line[index + len] == '_'
+		|| (len != 0 && ft_isdigit(line[index + len])))
+		len++;
+	return (len);
+}
+
+/* ************************************************************************** */
+
+int	add_varsub(char *line, int index, t_lexer_opt *opt)
+{
+	const int	len = get_var_len(line, index + 1);
+	char		*value;
+
+	if (len == 0)
+		return (add_char(opt, "$"));
+	vec_fill(&g_data.tmp, FIXED_LEN, &line[index + 1], len);
+	value = getenv(vec_get_str(&g_data.tmp, 0));
+	vec_delete(&g_data.tmp);
+	if (value)
+	{
+		vec_fill(get_line(), DEFAULT, value);
+		opt->index_line += ft_strlen(value);
+	}
+	else
+		add_char(opt, "\0");
+	return (len + 1);
+}
+
+/** $VAR */
+int varsub(char *line, int index, t_lexer_opt *opt)
+{
+	opt->index_line++;
+	add_instr_or_arg(opt);
+	opt->index_line--;
+	return (add_varsub(line, index, opt));
+}
