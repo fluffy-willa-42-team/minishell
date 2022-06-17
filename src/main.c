1bb87a858f8d073c7a1cc5b0ec9b7d34d5e31d06
willaCS <arthur.willems.prive@gmail.com>
2022-06-17 11:13:26 +0200

    moved empty instr

--- src/main.c
+++ src/main.c
@@ -6,11 +6,12 @@
 /*   By: awillems <awillems@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/27 09:36:24 by awillems          #+#    #+#             */
-/*   Updated: 2022/06/17 11:03:59 by awillems         ###   ########.fr       */
+/*   Updated: 2022/06/17 11:12:41 by awillems         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+#include "vec_utils.h"
 
 #include <stdio.h>
 #include <readline/readline.h>
@@ -18,7 +19,6 @@
 
 void	set_sigaction(void);
 void	line_lexer(char *line);
-void	empty_g_data(void);
 
 t_minishell	g_data;
 
@@ -33,6 +33,32 @@ void	init_data(void)
 	g_data.lexed_instr = vec_init(sizeof(t_instr));
 	g_data.lexed_instr.rate = 8;
 	g_data.tmp = vec_init(sizeof(char));
+}
+
+int	change_str_to_int(char **input, int *output)
+{
+	(void) input;
+	*output = 0;
+	return (1);
+}
+
+/**
+ * @brief Empties
+ * 
+ */
+void	empty_g_data(void)
+{
+	size_t	i;
+
+	i = 0;
+	vec_delete(get_line());
+	while (i < g_data.lexed_instr.content_len)
+	{
+		vec_cast(get_instr_arg(i), sizeof(int), change_str_to_int);
+		vec_delete(get_instr_arg(i));
+		get_instr(i)->type = 0;
+		i++;
+	}
 }
 
 /**
