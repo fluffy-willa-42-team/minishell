22f7d7f33784b4af15aa98e9427cafbd007d61c2
willaCS <arthur.willems.prive@gmail.com>
2022-06-21 09:30:55 +0200

    exe print

--- src/3_executor/executor.c
+++ src/3_executor/executor.c
@@ -6,13 +6,29 @@
 /*   By: awillems <awillems@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/20 15:07:10 by awillems          #+#    #+#             */
-/*   Updated: 2022/06/20 15:25:25 by awillems         ###   ########.fr       */
+/*   Updated: 2022/06/21 09:13:32 by awillems         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+#include "vec_utils.h"
 
 void	line_executor(void)
 {
 	printf("\e[0;36m0=====-----	EXECUTION	-----=====0\n\e[0m");
+	for (size_t i = 0; i < get_instr_list()->content_len; i++)
+	{
+		if ( get_instr(i)->type != 1)
+			continue ;
+		size_t j = 0;
+		printf("%d => [%s", get_instr(i)->file_descriptor[0], get_instr_arg_elem(i, j));
+		j++;
+		while (j < get_instr_arg(i)->content_len)
+		{
+			printf(", %s", get_instr_arg_elem(i, j));
+			j++;
+		}
+		printf("] => %d\n", get_instr(i)->file_descriptor[1]);
+	}
+	printf("\e[0;36m0=====-----	END	-----=====0\n\e[0m");
 }
