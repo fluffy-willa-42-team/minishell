b5a70913026ac0a725527ff236d70a38a913c5fd
willaCS <arthur.willems.prive@gmail.com>
2022-06-23 10:31:56 +0200

    print_cmd

--- src/4_executor/executor.c
+++ src/4_executor/executor.c
@@ -6,21 +6,26 @@
 /*   By: awillems <awillems@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/20 15:07:10 by awillems          #+#    #+#             */
-/*   Updated: 2022/06/23 09:31:46 by awillems         ###   ########.fr       */
+/*   Updated: 2022/06/23 10:23:02 by awillems         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 #include "vec_utils.h"
 
+void	print_cmd(size_t i)
+{
+	size_t j = 0;
+	printf("%d => [%s", get_instr(i)->file_descriptor[0], get_instr_arg_elem(i, j));
+	j++;
+	while (j < get_instr_arg(i)->content_len)
+		printf(", %s", get_instr_arg_elem(i, j++));
+	printf("] => %d\n", get_instr(i)->file_descriptor[1]);
+}
+
 void	execute_cmd(size_t i)
 {
-		size_t j = 0;
-		printf("%d => [%s", get_instr(i)->file_descriptor[0], get_instr_arg_elem(i, j));
-		j++;
-		while (j < get_instr_arg(i)->content_len)
-			printf(", %s", get_instr_arg_elem(i, j++));
-		printf("] => %d\n", get_instr(i)->file_descriptor[1]);
+	print_cmd(i);
 }
 
 void	line_executor(void)
