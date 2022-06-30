708a487f2f6ab4300c6f0a334fbe2374c03dee8c
willaCS <arthur.willems.prive@gmail.com>
2022-06-30 12:54:05 +0200

    changed color of print debug

--- src/0_utils/print_debug_str.c
+++ src/0_utils/print_debug_str.c
@@ -6,7 +6,7 @@
 /*   By: awillems <awillems@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/30 11:03:34 by awillems          #+#    #+#             */
-/*   Updated: 2022/06/30 11:13:17 by awillems         ###   ########.fr       */
+/*   Updated: 2022/06/30 12:53:43 by awillems         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,5 +22,5 @@ void	print_debug_sep(char *str)
 void	print_debug(char *str)
 {
 	if (DEBUG_PRINT)
-		printf("%s%s\n%s", "\e[0;36m", str, "\e[0m");
+		printf("%s%s\n%s", "\e[0;33m", str, "\e[0m");
 }
