1ddcf18a1ebe1c1a7c67188d030490379527f03a
willaCS <arthur.willems.prive@gmail.com>
2022-06-30 10:09:40 +0200

    [CHANGE] parse check added

--- src/2_pars_check/parse_check.c
+++ src/2_pars_check/parse_check.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
-/*                                                                            */
+/*                                                                          */
 /*                                                        :::      ::::::::   */
 /*   pars_check.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: awillems <awillems@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/22 12:55:46 by mahadad           #+#    #+#             */
-/*   Updated: 2022/06/27 09:25:50 by awillems         ###   ########.fr       */
+/*   Updated: 2022/06/30 09:50:26 by awillems         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,13 +28,19 @@
    DEFAULT  | dflt_char
 */
 
-void	line_pars_check(int nb_instr)
+int	line_parse_check(int nb_instr)
 {
-	if (!DEBUG_PRINT)
-      return ;
-   printf("\e[0;36m0=====-----	PARSER CHECK	-----=====0\n\e[0m");
+	if (DEBUG_PRINT)
+		printf("\e[0;36m0=====-----	PARSER CHECK	-----=====0\n\e[0m");
 	for (int i = 0; i < nb_instr; i++)
 	{
-		printf("[%d]\n", get_instr(i)->type);
+		if ((get_instr(i)->type == 2 || get_instr(i)->type == 3) && i + 1 < nb_instr
+				&& (get_instr(i + 1)->type == 2 || get_instr(i + 1)->type == 3))
+		{
+			printf("Parsing Error\n");
+			return (0);
+		}
 	}
+	printf("[OK]\n");
+	return (1);
 }
