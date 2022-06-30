f82ac0549f1e64d49d3a56025acc3bd4b9be5279
willaCS <arthur.willems.prive@gmail.com>
2022-06-30 10:52:26 +0200

    [NORM] Utils with norm

--- src/0_utils/print_instr.c
+++ src/0_utils/print_instr.c
@@ -6,7 +6,7 @@
 /*   By: awillems <awillems@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/02 11:20:44 by awillems          #+#    #+#             */
-/*   Updated: 2022/06/28 13:58:37 by awillems         ###   ########.fr       */
+/*   Updated: 2022/06/30 10:45:01 by awillems         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,18 +24,18 @@ void	print_instr(size_t len, int type)
 	{
 		if (type == -1 || get_instr(ind)->type == type)
 		{
-		printf("    {\n");
-		printf("        type: %d,\n", get_instr(ind)->type);
-		printf("        fd: [%d, %d],\n",
-			get_instr(ind)->fds[0],
-			get_instr(ind)->fds[1]);
-		printf("        err: %d,\n", get_instr(ind)->err);
-		printf("        arg: [\n");
-		arg = -1;
-		while (++arg < get_instr_arg(ind)->len)
-			printf("            \"%s\",\n", get_instr_arg_elem(ind, arg));
-		printf("        ],\n");
-		printf("    },\n");
+			printf("    {\n");
+			printf("        type: %d,\n", get_instr(ind)->type);
+			printf("        fd: [%d, %d],\n",
+				get_instr(ind)->fds[0],
+				get_instr(ind)->fds[1]);
+			printf("        err: %d,\n", get_instr(ind)->err);
+			printf("        arg: [\n");
+			arg = -1;
+			while (++arg < get_instr_arg(ind)->len)
+				printf("            \"%s\",\n", get_instr_arg_elem(ind, arg));
+			printf("        ],\n");
+			printf("    },\n");
 		}
 	}
 	printf("]\n");
