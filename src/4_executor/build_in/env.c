03ca57c74701623fcf40c92ba0372092d661d4d0
willaCS <arthur.willems.prive@gmail.com>
2022-07-01 17:32:51 +0200

    [RENAME] set_env.c -> env.c

--- src/4_executor/build_in/env.c
+++ src/4_executor/build_in/env.c
@@ -1,37 +1,21 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   set_env.c                                          :+:      :+:    :+:   */
+/*   env.c                                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mahadad <mahadad@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/30 15:51:25 by mahadad           #+#    #+#             */
-/*   Updated: 2022/06/30 17:00:20 by mahadad          ###   ########.fr       */
+/*   Updated: 2022/07/01 13:37:07 by mahadad          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "vec_utils.h"
 #include "env_utils.h"
 
-t_vec	*env(char **env)
+int	env(char **arg)
 {
-	size_t	len;
-
-	if (!env)
-		return (NULL);
-	env++;
-	len = 0;
-	while (env[0][len] && env[0][len] != '=')
-		len++;
-	vec_delete(&g_data.tmp);
-	vec_fill(&g_data.tmp, FIXED_LEN, len, *env);
-	while (*env)
-	{
-		printf("env set [%s]\n", *env);
-		env_set((char *)g_data.tmp.buffer, *env, 1);//TODO ENV mettre seulement le nom de la variable.
-		updt_env();
-		print_env();
-		env++;
-	}
-	return NULL;
+	(void)arg;
+	print_env();
+	return (1);
 }
