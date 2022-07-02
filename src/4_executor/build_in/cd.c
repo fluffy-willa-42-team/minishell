aca2af867a884eb0afc8d963018ac5ec1b81b0e1
willaCS <arthur.willems.prive@gmail.com>
2022-07-02 11:46:58 +0200

    x

--- src/4_executor/build_in/cd.c
+++ src/4_executor/build_in/cd.c
@@ -6,11 +6,17 @@
 /*   By: awillems <awillems@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/02 10:06:29 by awillems          #+#    #+#             */
-/*   Updated: 2022/07/02 11:11:21 by awillems         ###   ########.fr       */
+/*   Updated: 2022/07/02 11:41:29 by awillems         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+// #include <sys/types.h>
+
+#include <dirent.h>
+
+//opendir, readdir, closedir,
 
 //CHECK IF EXIST
 //CHECK IF FOLDER
@@ -20,6 +26,11 @@ int	msh_cd(char **args)
 {
 	int	i;
 
+	// DIR *res = opendir(args[1]);
+	// if (!res)
+	// 	return (0);
+	// closedir(res);
+	// (void) res;
 	i = -1;
 	while (args[++i])
 	{
