95fa61148c5f23ce6d7b8c716fe91d7f49dde77c
matthew-dreemurr <hadadmat@gmail.com>
2022-07-03 13:13:34 +0200

    [WIP] build in

--- src/msh_exit.c
+++ src/msh_exit.c
@@ -6,7 +6,7 @@
 /*   By: mahadad <mahadad@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/01 12:03:50 by awillems          #+#    #+#             */
-/*   Updated: 2022/07/03 09:59:49 by mahadad          ###   ########.fr       */
+/*   Updated: 2022/07/03 13:11:43 by mahadad          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,6 @@
 int	msh_exit(int return_value, int exit_code, char *message, const char *fct)
 {
 	g_data.last_exit_code = exit_code;
-	//TODO check if we can free in fork child ?
-	//TODO free all vec.
 	if (DEBUG_PRINT)
 		printf("%s: ", fct);
 	if (exit_code)
