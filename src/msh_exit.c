7ad54b2c65e9e8ed241be4d15a426d0e3f4a65c9
matthew-dreemurr <hadadmat@gmail.com>
2022-07-03 14:10:47 +0200

    [WIP]

--- src/msh_exit.c
+++ src/msh_exit.c
@@ -6,18 +6,23 @@
 /*   By: mahadad <mahadad@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/01 12:03:50 by awillems          #+#    #+#             */
-/*   Updated: 2022/07/03 13:11:43 by mahadad          ###   ########.fr       */
+/*   Updated: 2022/07/03 14:05:22 by mahadad          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+#include "lib_put_fd.h"
 
 int	msh_exit(int return_value, int exit_code, char *message, const char *fct)
 {
 	g_data.last_exit_code = exit_code;
 	if (DEBUG_PRINT)
-		printf("%s: ", fct);
+		printf("%s:\n", fct);
 	if (exit_code)
-		printf("Error: %s\n", message);
+	{
+		ft_putstr_fd("minishell: ", STDOUT_FILENO);
+		ft_putstr_fd(message, STDOUT_FILENO);
+		ft_putstr_fd("\n", STDOUT_FILENO);
+	}
 	return (return_value);
 }
